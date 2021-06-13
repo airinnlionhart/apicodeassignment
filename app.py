45195f8df205ee0351471490e5845f73fdc7090d
alenharta <alenharta@alenhartas-air.myfiosgateway.com>
2021-06-13 15:34:46 -0400

    ready to merge with main

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, render_template, request, jsonify
 import models
 import json
 from logic import GetApplicants
 from os import listdir
 from os.path import isfile, join
-
-
+from random import randint
 
 Debug = True
 
@@ -14,14 +13,14 @@ app = Flask(__name__)
 
 @app.before_request
 def before_request():
-    """Conect to the datebase before each request"""
+    """Connect to the database before each request"""
     db = models.DATABASE
     db.connect()
 
 
 @app.after_request
-def after_reqest(response):
-    """Close the datebase after each request"""
+def after_request(response):
+    """Close the database after each request"""
     models.DATABASE.close()
     return response
 
@@ -31,17 +30,39 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/add_question', methods=("GET", "POST"))
+def add():
+    """route to post questions"""
+    for items in json.loads(json.dumps(request.get_json())):
+        questionid = items['Id']
+        question = items['Question']
+        answer = items['Answer']
+        models.Questions.add(questionid=questionid, userid=1000,
+                             question=question,
+                             answer=answer)
+
+    return "added questions"
+
+
+@app.route('/get_qualified', methods=("GET", "POST"))
+def get_qualified():
+    """route to post and get back qualified applicants"""
+
+    new_file = request.get_json()[:]
+    get_json = json.dumps(new_file)
+    applicants = GetApplicants.qualified(json.loads(get_json))
+    return json.dumps(applicants)
+
+
 @app.route('/questions', methods=('GET', 'POST'))
 def questions_import():
     """Html import page for import applicants """
     if request.method == "POST":
         file = request.form['questions']
-        list_file = file[1:-1]
-        new_file = json.loads(list_file)
-        get_json = json.dumps(new_file)
-        models.Questions.add(questionid=json.loads(get_json.lower())['id'], userid=10000,
-                             question=json.loads(get_json.lower())['question'],
-                             answer=json.loads(get_json.lower())['answer'])
+        for item in json.loads(file):
+            models.Questions.add(questionid=item['Id'], userid=1000,
+                             question=item['Question'],
+                             answer=item['Answer'])
     return render_template('questionimported.html')
 
 
@@ -57,18 +78,42 @@ def applicant_import():
     return render_template('applicantimport.html')
 
 
-
 @app.route('/file/get_applicants', methods=['GET'])
 def get_apps():
     """Get applicants from a json file"""
+    if models.Applicants.table_exists():
+        pass
+    else:
+        models.DATABASE.create_tables([models.Applicants], safe=True)
+    if models.AppQuestions.table_exists():
+        pass
+    else:
+        models.DATABASE.create_tables([models.AppQuestions], safe=True)
     jsonFile = open('applicants.json', )
     file = json.load(jsonFile)
-    applicants = GetApplicants.qualified(file)
+    for applicant in file:
+        appid = randint(2000, 100000)
+        try:
+            models.Applicants.add(appid=appid, name=applicant['Name'])
+            for questions in applicant['Questions']:
+                models.AppQuestions.add(appid=appid, questionid=questions['Id'], answer=questions['Answer'])
+                GetApplicants.qualified_applicants(appid)
+
+        except:
+            """For the off chance that random number comes up with the same number"""
+            appid = randint(2000, 100000)
+            models.Applicants.add(appid=appid, name=applicant['Name'])
+            for questions in applicant['Questions']:
+                models.AppQuestions.add(appid=appid, questionid=questions['Id'], answer=questions['Answer'])
+                GetApplicants.qualified_applicants(appid)
+
+    qualified_applicants = models.Applicants.select().dicts().where(models.Applicants.qualified == True)
+
     jsonFile.close()
-    return json.dumps(applicants)
+    return jsonify(str([items for items in qualified_applicants]))
 
 
-@app.route('/files', methods=['GET'])
+@app.route('/infolder', methods=['GET'])
 def form_file():
     if models.Questions.table_exists():
         pass
@@ -86,18 +131,18 @@ def form_file():
         questions_json.append(file)
 
     all_questions = json.dumps(questions_json[0])
+    print(json.loads(all_questions))
     for question in json.loads(all_questions):
-            try:
-                models.Questions.add(questionid=question['id'], userid=10000, question=question['question'],
-                                     answer=question['answer'])
-            except:
-                pass
+        try:
+            models.Questions.add(questionid=question['Id'], userid=10000, question=question['Question'],
+                                 answer=question['Answer'])
+        except:
+            pass
 
     for files in only_app_files:
         jsonFile = open('applicantfile/' + files, )
         file = json.load(jsonFile)
         applicants = GetApplicants.qualified(file)
         good_applicant.append(applicants)
-
 
     return json.dumps(good_applicant)
--- logic.py
+++ logic.py
@@ -1,18 +1,39 @@
 import json
 import models
 
+
 class GetApplicants:
     @staticmethod
     def qualified(file):
         ifTrue = True
         good_applicants = []
         for applicants in file:
-            for question in applicants['questions']:
-                question_field = models.Questions.get(models.Questions.questionid == question['id']).answer
-                if question_field.lower().strip() == question['answer'].lower().strip():
+            for question in applicants['Questions']:
+                question_field = models.Questions.get(models.Questions.questionid == question['Id']).answer
+                if question_field.lower().strip() == question['Answer'].lower().strip():
                     ifTrue = True
                 else:
                     ifTrue = False
             if ifTrue:
                 good_applicants.append(applicants)
         return good_applicants
+
+    @staticmethod
+    def qualified_applicants(appid):
+        if models.Applicants.table_exists():
+            pass
+        else:
+            models.DATABASE.create_tables([models.Applicants], safe=True)
+        if models.AppQuestions.table_exists():
+            pass
+        else:
+            models.DATABASE.create_tables([models.AppQuestions], safe=True)
+
+        apps_questions = models.AppQuestions.select().dicts().where(models.AppQuestions.appid == appid)
+        for answers in apps_questions:
+            question_field = models.Questions.get(models.Questions.questionid == answers['questionid']).answer
+            if str(question_field.lower().strip()) != str(answers['answer'].lower().strip()):
+                models.Applicants.update(qualified=False).where(models.Applicants.appid == appid).execute()
+            else:
+                pass
+        return "updated"
