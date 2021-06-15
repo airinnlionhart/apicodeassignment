d36e62f6f5ca5adc324a1f0439b8c6282d66d3b2
Aaron Lenhart <49368013+airinnlionhart@users.noreply.github.com>
2021-06-15 09:18:00 -0400

    fixed logic

--- logic.py
+++ logic.py
@@ -1,33 +1,29 @@
 import models
 
-
 class GetApplicants:
     @staticmethod
     def qualified(file):
-        ifTrue = True
         good_applicants = []
+        number_of_questions = len(models.Questions.select().dicts())
         for applicants in file:
             try:
-                for question in applicants['Questions']:
-                    try:
-                        question_field = models.Questions.get(models.Questions.questionid == question['Id']).answer
-                    except:
-                        print('No Applicants match')
-                if question_field.lower().strip() == question['Answer'].lower().strip():
-                    ifTrue = True
-                else:
-                    ifTrue = False
-            except:
-                for question in applicants['questions']:
-                    try:
-                        question_field = models.Questions.get(models.Questions.questionid == question['id']).answer
-                    except:
-                        print('No Applicants match')
-                if question_field.lower().strip() == question['answer'].lower().strip():
-                    ifTrue = True
-                else:
-                    ifTrue = False
-            if ifTrue:
+                print(len(applicants['Questions']))
                 good_applicants.append(applicants)
+                if number_of_questions != len(applicants['Questions']):
+                    good_applicants.remove(applicants)
+                else:
+                    for question in applicants['Questions']:
+                        question_field = models.Questions.get(models.Questions.questionid == question['Id']).answer
+                        if question_field.lower().strip() != question['Answer'].lower().strip():
+                            good_applicants.remove(applicants)
+            except:
+                print(len(applicants['questions']))
+                good_applicants.append(applicants)
+                if number_of_questions != len(applicants['questions']):
+                    good_applicants.remove(applicants)
+                else:
+                    for question in applicants['questions']:
+                        question_field = models.Questions.get(models.Questions.questionid == question['id']).answer
+                        if question_field.lower().strip() != question['answer'].lower().strip():
+                            good_applicants.remove(applicants)
         return good_applicants
-
