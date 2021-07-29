3c1efc6d0269906c79087a747afab68ea7555b0f
Aaron Lenhart <49368013+airinnlionhart@users.noreply.github.com>
2021-07-29 14:40:15 -0400

    Update logic.py

--- logic.py
+++ logic.py
@@ -3,27 +3,19 @@ import models
 class GetApplicants:
     @staticmethod
     def qualified(file):
+        """
+        Grabs a json file of applicants and compare questions to answers and return only good applicants
+        """
         good_applicants = []
         number_of_questions = len(models.Questions.select().dicts())
         for applicants in file:
-            try:
-                good_applicants.append(applicants)
-                if number_of_questions != len(applicants['Questions']):
-                    good_applicants.remove(applicants)
-                else:
-                    for question in applicants['Questions']:
-                        question_field = models.Questions.get(models.Questions.questionid == question['Id']).answer
-                        if question_field.lower().strip() != question['Answer'].lower().strip():
-                            good_applicants.remove(applicants)
-                            break
-            except:
-                good_applicants.append(applicants)
-                if number_of_questions != len(applicants['questions']):
-                    good_applicants.remove(applicants)
-                else:
-                    for question in applicants['questions']:
-                        question_field = models.Questions.get(models.Questions.questionid == question['id']).answer
-                        if question_field.lower().strip() != question['answer'].lower().strip():
-                            good_applicants.remove(applicants)
-                            break
+            good_applicants.append(applicants)
+            if number_of_questions != len(applicants['Questions']):
+                good_applicants.remove(applicants)
+            else:
+                for question in applicants['Questions']:
+                    question_field = models.Questions.get(models.Questions.questionid == question['Id']).answer
+                    if question_field.lower().strip() != question['Answer'].lower().strip():
+                        good_applicants.remove(applicants)
+                        break
         return good_applicants
